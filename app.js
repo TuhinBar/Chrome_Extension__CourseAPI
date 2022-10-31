435bf4120d8eee55076a21551677197c66959013
TuhinBar <tuhinbr99@gmail.com>
2022-10-31 19:23:40 +0530

    Refactored the code sample
    Fixed some stylings, removed unnecessary styles. Fixed some repetitions on html. Made the javascript more functional by making functions for each tasks like populating divs, displaying date and the main function which include fetch to create the request to the API endpoint to get all the courses links and other details.

--- app.js
+++ app.js
@@ -1,37 +1,77 @@
-let today = new Date();
-let date = today.getDate()+'-'+ (today.getMonth()+1)+'-'+today.getFullYear();
+// creating functions
 
-const free_course_endpoint =
-  "https://freecoursesfetch.herokuapp.com/";
-
-// Fetch free courses
-fetch(free_course_endpoint)
-    .then(data => data.json()).then(courses => {
-
-        const c_name =[];
-        const c_url =[];
-        for(let i=0;i<=9;i++){
-            c_name.push(courses.results[i].name);
-            if(courses.results[i].url.includes("http://click.linksynergy.com/fs-bin/click?id=bnwWbXPyqPU&subid=&offerid=323058.1&type=10&tmpid=14537&RD_PARM1=")){
-                c_url.push(courses.results[i].url.split("http://click.linksynergy.com/fs-bin/click?id=bnwWbXPyqPU&subid=&offerid=323058.1&type=10&tmpid=14537&RD_PARM1=")[1]);
-            } else{
-                c_url.push(courses.results[i].url);
-            }
-        }
-        const C_name_element = document.querySelectorAll('.course');
-        for(let i=0;i<C_name_element.length;i++){
-            C_name_element[i].innerHTML = c_name[i];
-            C_name_element[i].href = c_url[i];
-        }
-    });
-
-setInterval(displayTime,1000);
-
-function displayTime(){
-  document.getElementById('date').innerText = today.toLocaleString('en-US',{
-    dateStyle:'full'
-  });
-
+// function to populate divs for courses
+function createCourseBody(){
+  let items = document.querySelector(".items");
+console.log(items);
+for(let i = 1; i <= 10; i++){
+  items.innerHTML += `<div class="items-head">
+  <p>COURSE: ${i}</p>
+  <hr />
+</div>
+<div class="items-body">
+  <div class="items-body-content">
+    <a id="course1" class="course" href="#" target="_blank"
+      >Loading..</a
+    >
+  </div>
+</div>`;
+}
 }
 
-displayTime();
+// Main function to fetch courses
+function getCourses() {
+  // API endpoint to get courses links and details
+  const coursesEndpoint = "https://freecoursesfetch.herokuapp.com/";
+
+  // Fetch free courses
+  fetch(coursesEndpoint)
+    .then((data) => data.json())
+    .then((courses) => {
+      // Storing courses name and links in arrays
+      const courseNames = [];
+      const courseUrls = [];
+
+      // The course urls from the API contains a ad redirection links so looping through the course urls and removing the ad links
+      for (let i = 0; i <= 9; i++) {
+        courseNames.push(courses.results[i].name);
+        if (
+          courses.results[i].url.includes(
+            "http://click.linksynergy.com/fs-bin/click?id=bnwWbXPyqPU&subid=&offerid=323058.1&type=10&tmpid=14537&RD_PARM1="
+          )
+        ) {
+          courseUrls.push(
+            courses.results[i].url.split(
+              "http://click.linksynergy.com/fs-bin/click?id=bnwWbXPyqPU&subid=&offerid=323058.1&type=10&tmpid=14537&RD_PARM1="
+            )[1]
+          );
+        } else {
+          courseUrls.push(courses.results[i].url);
+        }
+      }
+
+      // Grabbing the container to populate the courses
+      const coursesContainer = document.querySelectorAll(".course");
+      // Looping through the courses container and populating the courses name and links
+      for (let i = 0; i < coursesContainer.length; i++) {
+        coursesContainer[i].innerHTML = courseNames[i];
+        coursesContainer[i].href = courseUrls[i];
+      }
+    });
+}
+
+// function to display date
+function displayDate() {
+  let today = new Date();
+  document.getElementById("date").innerText = today.toLocaleString("en-US", {
+    dateStyle: "full",
+  });
+}
+
+// Calling functions
+createCourseBody();
+displayDate();
+getCourses();
+
+
+
